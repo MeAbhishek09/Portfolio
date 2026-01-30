57bbd35842a15ca39c432be3c15162a644934716
Abhishek <abhishek1322005@gmail.com>
2026-01-30 13:46:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -13,10 +14,6 @@ export default defineConfig({
   
 })
 
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
 
 // // https://vite.dev/config/
 // export default defineConfig(({ mode }) => ({
