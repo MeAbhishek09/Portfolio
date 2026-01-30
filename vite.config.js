d87595db6cac9843ec9e12a983fcb5ec57181174
Abhishek <abhishek1322005@gmail.com>
2026-01-30 13:38:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   
 })
 
